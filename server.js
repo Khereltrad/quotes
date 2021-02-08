fc88d5955bde201219027d3032b4f5b2928bbd74
Claudio René Aránguiz Herrera <claudioarnguiz@yahoo.com>
2021-02-08 12:08:37 -0300

    avance inicial a tarea quotes de sequelize

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+
+// app.use(express.static(__dirname+"/public"));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+//! enrrutamientos
+app.get("/quotes", (req, res) => {
+    res.render("quotes",{quotes:quotes});
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+const quotes = [
+    {
+        who: "",
+        quote:""
+    },
+    {
+        who: "",
+        quote:""
+    },
+    {
+        who: "",
+        quote:""
+    },
+    {
+        who: "",
+        quote:""
+    },
+    {
+        who: "",
+        quote:""
+    },
+    {
+        who: "",
+        quote:""
+    },
+
+];
+
+
+
+//? app.use(require('./routes/NOMBREDELARCHIVOQUEESTAENROUTES'));   //! Llamado de rutas
+
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
+
+
+//?✌       EN EL TERMINAL
+
+//^         node server.js
+//          navegador vistar    localhost:8000/
+       
