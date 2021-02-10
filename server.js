d89ff6575c39697fd0ef6624a30a52cbc7896eb5
Claudio René Aránguiz Herrera <claudioarnguiz@yahoo.com>
2021-02-10 10:46:54 -0300

    avance citas falta solo trabajo en estilos

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const {Router} = require('express');
 const app = express();
+const rt = Router();
 const port = 8000;
 
 
@@ -7,8 +9,12 @@ app.use(express.static(__dirname+"/public"));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
 //! enrrutamientos
 app.get("/quotes", (req, res) => {
+
     res.render("quotes",{quotes:quotes});
 });
 
@@ -16,59 +22,77 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.post("/quotes", (req, res) => {
+    quotes.push({
+        who: req.body.author,
+        quote:req.body.cita
+        // hour:"",
+        // when:""
+    });
+    
+    res.redirect("/quotes");
+});
+
 const quotes = [
     {
-        who: "",
-        quote:"",
-        hour:"",
-        when:""
+        who: "sdfsfsd",
+        quote:"rtet",
+        // hour:"",
+        // when:""
     },
-    {
-        who: "",
-        quote:"",
-        hour:"",
-        when:""
-    },
-    {
-        who: "",
-        quote:"",
-        hour:"",
-        when:""
-    },
-    {
-        who: "",
-        quote:"",
-        hour:"",
-        when:""
-    },
-    {
-        who: "",
-        quote:"",
-        hour:"",
-        when:""
-    },
-    {
-        who: "",
-        quote:"",
-        hour:"",
-        when:""
-    },
+    // {
+    //     who: "",
+    //     quote:"",
+    //     hour:"",
+    //     when:""
+    // },
+    // {
+    //     who: "",
+    //     quote:"",
+    //     hour:"",
+    //     when:""
+    // },
+    // {
+    //     who: "",
+    //     quote:"",
+    //     hour:"",
+    //     when:""
+    // },
+    // {
+    //     who: "",
+    //     quote:"",
+    //     hour:"",
+    //     when:""
+    // },
+    // {
+    //     who: "",
+    //     quote:"",
+    //     hour:"",
+    //     when:""
+    // },
 
 ];
 
-app.post("/quotes", function(req,res){
-    const new_cuote = {
-        who:    req.body.author,
-    }
-});
 
-//? app.use(require('./routes/NOMBREDELARCHIVOQUEESTAENROUTES'));   //! Llamado de rutas
+// app.get('/quotes', async(req,res) => {
+//     await new_quote.create({
+//         who:    req.body.author,
+//         quotes: req.body.cita
+//     });
+//     console.log('hay un nuevo posteo');
+    
+//     res.redirect('quotes.ejs');
+// };
+
+
+// app.post("/quotes", async (req,res) => {
+//     const new_cuote = await quote.create({
+//         who:    req.body.author,
+//         quotes:  req.body.cita
+//     });
+//     console.log('hay un nuevo posteo');
+    
+//     res.redirect('quotes.ejs');
+// });
 
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
-
-
-//?✌       EN EL TERMINAL
-
-//^         node server.js
-//          navegador vistar    localhost:8000/
-       
