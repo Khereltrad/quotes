2a007b53749c932bbab6ed134bd15392374268cf
Claudio René Aránguiz Herrera <claudioarnguiz@yahoo.com>
2021-02-16 12:37:46 -0300

    boton Eliminar borra fila seleccionada, aun no borra desde base de datos

--- routes/routes.js
+++ routes/routes.js
@@ -3,50 +3,26 @@ const rt = Router();
 const { Cita } = require('../db');
 
 
-// para mostrar los paises
-// rt.get('/', async (req, res) => {
-//   const quotes = await Cita.findAll();
-//   res.render('index.ejs', {quotes: quotes})
-// });
-
 rt.get("/", async (req, res) => {
   const quotes = await Cita.findAll();
-  res.render("index",{quotes:quotes});
-});  //* enrrutamiento de raiz 
+  res.render("index", { quotes: quotes });
+});
 
 rt.post('/quotes', async (req, res) => {
-  console.log('Formulario : '+req.body.who, req.body.quote);
   const new_quote = await Cita.create({
     who: req.body.who,
     quote: req.body.quote
   });
 
   const quotes = await Cita.findAll();
-
-  res.render('index.ejs', {quotes: quotes});
+  res.render('index.ejs', { quotes: quotes });
 });
 
-
-// rt.get('/quotes', async(req,res) => {
-//     await new_quote.create({
-//         who:    req.body.author,
-//         quotes: req.body.cita
-//     });
-//     console.log('hay un nuevo posteo');
-    
-//     res.redirect('quotes.ejs');
-// };
-
-// rt.post("/quotes", async (req,res) => {
-//     const new_cuote = await quote.create({
-//         who:    req.body.author,
-//         quotes:  req.body.cita
-//     });
-//     console.log('hay un nuevo posteo');
-    
-//     res.redirect('quotes.ejs');
-// });
-
+rt.get('/borrar/:id', async (req,res) =>{
+  const quote = await Cita.findByPk(req.params.id);
+  
+  res.send('OK');
+});
 
 
 module.exports = rt;
